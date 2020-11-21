5014be25db59902f96d6904973f164099e0e650e
Rachel Wagschal <rawagschal@gmail.com>
2020-11-21 14:18:20 -0800

    update nav

---
+++ controllers/home-routes.js
@@ -0,0 +1,8 @@
+// HOMEPAGE ROUTE
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 // const emailRoute = require('./email-route');
 const loginRoute = require('./login-route');
 
-
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 // router.use('/email', emailRoute);
 router.use('/login', loginRoute);
