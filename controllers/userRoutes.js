70d790d73f54d0e5e229694c261f4dc030e61be3
Kim Mulligan <kimmariemulligan@gmail.com>
2020-11-22 00:13:20 -0800

    added 5 seed items in index.js of the seeds folder

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -10,8 +10,20 @@ const userController = require('./api/userController')
 
 
 
+//api routes
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
+
+//html routes item and user
+
+// const userRoutes = require('./userRoutes');
+// router.use('/user', userRoutes);
+
+// const itemRoutes = require('./itemRoutes');
+// router.use('/item', itemRoutes);
 
 
-
-
+router.use((req, res) => {
+    res.status(404).end();
+  });
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -3,15 +3,54 @@ const { Item } = require('../models');
 
 const itemData = [{
      
-  "itemType": "Chair",
-  "price": "100",
-  "name": "kids chair",
-  "era": "art deco",
-  "dimensions": "24 in x 30 in x 16 in",
-  "photoUrl": "abcdsf.com"
+  "itemType": "Dresser with Mirror",
+  "price": "299",
+  "name": "Exquisite 1900's Serpentine Tiger Oak Dresser with Mirror",
+  "era": "Victorian",
+  "dimensions": "Dresser: H:27in W:19.5in x L:34in; with Mirror: H:47in", 
+  "photoUrl": "dresserwmirror.com"
 
-}
+},
+{
+ "itemType": "Chair ",
+  "price": "199",
+  "name": "Strong Brown Chair",
+  "era": "Mid-Century",
+  "dimensions": "Chair: H:32in W:24in x L:24in; Seat: H:19in", 
+  "photoUrl": "brownchair.com"
+},
+{
+     
+  "itemType": "Buffet Table with Hutch",
+  "price": "799",
+  "name": "Divine Antique Jacobean Oak Buffet Table with Hutch",
+  "era": "Victorian",
+  "dimensions": "Dresser: H:42in W:23in x L:52in;  Hutch: H:29in W:11in L:51in",
+  "photoUrl": "buffetwhutch.com"
+
+},
+{
+     
+  "itemType": "Bar",
+  "price": "389",
+  "name": "Classy Flip Top Bar on Wheels",
+  "era": "Victorian",
+  "dimensions": "Bar: H:34in W:21in x L:32in;  Extended: L:56", 
+  "photoUrl": "baronwheels.com"
+
+},
+{
+     
+  "itemType": "Dresser",
+  "price": "799",
+  "name": "Stunning Century Isabel Bombe Dresser",
+  "era": "Late 20th Century",
+  "dimensions": "H:36in W:23in x L:49in", 
+  "photoUrl": "bombedresser.com"
+
+},
+ 
 ]
 const seedItems = () => Item.bulkCreate(itemData);
 
-module.exports = seedUsers;
+module.exports = seedItems;
