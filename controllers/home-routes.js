0903ff13e6cb7f6ce82c382853466429b25e57c6
Rachel Wagschal <rawagschal@gmail.com>
2020-11-21 17:36:26 -0800

    attempt cart view to show user info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,37 @@
-// HOMEPAGE ROUTE
+// USER-FACING ROUTES
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    User.findAll({
+        attributes: [
+            'id',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'address',
+            'zip',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'first_name', 'last_name', 'username', 'email', 'address', 'zip']
+            }
+        ]
+    })
+    .then(dbUserData => {
+        console.log(dbUserData[0]);
+        const users = dbUserData.map(user => user.get({ plain: true}));
+        
+        res.render('cart', { users });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    
 });
 
 module.exports = router;
