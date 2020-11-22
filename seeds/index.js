3e1182ec5e3b01bae02fe9b012ae67bd69672587
Kim Mulligan <kimmariemulligan@gmail.com>
2020-11-22 00:37:51 -0800

    hosted pics on smugmug and added urls into seeds

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ const itemData = [{
   "name": "Exquisite 1900's Serpentine Tiger Oak Dresser with Mirror",
   "era": "Victorian",
   "dimensions": "Dresser: H:27in W:19.5in x L:34in; with Mirror: H:47in", 
-  "photoUrl": "dresserwmirror.com"
+  "photoUrl": "https://kimmulliganphoto.smugmug.com/Vintage-Wantings/n-R8qvCD/i-jwnbDWP"
 
 },
 {
@@ -17,7 +17,7 @@ const itemData = [{
   "name": "Strong Brown Chair",
   "era": "Mid-Century",
   "dimensions": "Chair: H:32in W:24in x L:24in; Seat: H:19in", 
-  "photoUrl": "brownchair.com"
+  "photoUrl": "https://kimmulliganphoto.smugmug.com/Vintage-Wantings/n-R8qvCD/i-9s3dptD"
 },
 {
      
@@ -26,7 +26,7 @@ const itemData = [{
   "name": "Divine Antique Jacobean Oak Buffet Table with Hutch",
   "era": "Victorian",
   "dimensions": "Dresser: H:42in W:23in x L:52in;  Hutch: H:29in W:11in L:51in",
-  "photoUrl": "buffetwhutch.com"
+  "photoUrl": "https://kimmulliganphoto.smugmug.com/Vintage-Wantings/n-R8qvCD/i-JzP7bgr"
 
 },
 {
@@ -36,7 +36,7 @@ const itemData = [{
   "name": "Classy Flip Top Bar on Wheels",
   "era": "Victorian",
   "dimensions": "Bar: H:34in W:21in x L:32in;  Extended: L:56", 
-  "photoUrl": "baronwheels.com"
+  "photoUrl": "https://kimmulliganphoto.smugmug.com/Vintage-Wantings/n-R8qvCD/i-p3BjcNz"
 
 },
 {
@@ -46,7 +46,7 @@ const itemData = [{
   "name": "Stunning Century Isabel Bombe Dresser",
   "era": "Late 20th Century",
   "dimensions": "H:36in W:23in x L:49in", 
-  "photoUrl": "bombedresser.com"
+  "photoUrl": "https://kimmulliganphoto.smugmug.com/Vintage-Wantings/n-R8qvCD/i-4jHwN9w"
 
 },
  
