a362c8280295cf7de22f258256399086ea1ec77b
Rachel Wagschal <rawagschal@gmail.com>
2020-11-24 18:11:06 -0800

    about page

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const homeRoute = require('./home-route');
 const apiRoutes = require('./api');
 // const loginRoute = require('./login-route');
-//const contactRoute = require('./contact-route');
+const aboutRoute = require('./about-route');
 const successRoute = require('./success-route');
 const cancelRoute = require('./cancel-route');
 
@@ -12,7 +12,7 @@ router.use('/', homeRoute);
 router.use('/success', successRoute);
 router.use('/cancel', cancelRoute);
 // router.use('/login', loginRoute);
-//router.use('/contact', contactRoute);
+router.use('/about', aboutRoute);
 
 
 
