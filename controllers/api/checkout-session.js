138589243cdb32a74e668aa6113774ef8635cd57
Rachel Wagschal <rawagschal@gmail.com>
2020-11-29 21:16:24 -0800

    redid success and cancel routes again

--- controllers/api/checkout-session.js
+++ controllers/api/checkout-session.js
@@ -25,8 +25,8 @@ router.post('/:id', async (req, res) => {
             },
         ],
         mode: 'payment',
-        success_url: '/success',
-        cancel_url: '/',
+        success_url: 'https://warm-crag-68291.herokuapp.com/success',
+        cancel_url: 'https://warm-crag-68291.herokuapp.com/',
     });
 
     res.json({ id: session.id });
